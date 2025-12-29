c7925a84a364e6158bb2c71186f0431c01cf63ab
Rachael DelNegro <sep8vb@virginia.edu>
2025-12-29 12:18:29 -0500

    Added shadcn/ui components for easy react building

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
+       "@": path.resolve(__dirname, "./src"),
       "@shared": fileURLToPath(new URL("../shared", import.meta.url))
     }
   },
